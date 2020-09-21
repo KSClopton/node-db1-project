481acb9518ce063a4fca9c3f016575c1f207ed99
KSClopton <kyle.clopton@outlook.com>
2020-09-20 21:56:55 -0400

    Finished all tasks

--- posts/posts-router.js
+++ posts/posts-router.js
@@ -11,18 +11,60 @@ router.get('/', (req, res) => {
      })
      .catch(handleError)
 })
-router.post('/:id', (req, res) => {
+// Checked!
+router.post('/', (req, res) => {
+    const newPost = req.body
     // INSERT INTO ACCOUNTS - need name and budget
-    db.insert().into('accounts').values(name = "Kyle", budget = 100)
-    .then(message => {
-        res.status(201).json()
-    })
+    if(!newPost.name || !newPost.budget){
+        res.status(404).json({message: "Please include both name and budget"})
+    }else{
+        db('accounts').insert(newPost, "id")
+        .then(message => {
+            res.status(201).json({message: "Post has been created"})
+        })
+        .catch(handleError)
+    }
 })
+
 router.delete('/:id', (req, res) => {
+    // find by ID
+    const {id} = req.params
     
+      db('accounts')
+      .where({id})
+      .del() // Will return the number of records deleted
+      .then(count => {
+        if(count > 0){
+            res.status(200).json({data: count})
+        }else{
+            res.status(404).json({message: "There was no record to delete"})
+        }
+       
+    })
+    .catch(error => {
+        res.status(500).json({message: "There was an error deleted the account"})
+    })  
 })
+
 router.put('/:id', (req, res) => {
-})
+    const {id} = req.params
+    const accountUpdate = req.body
+
+    db('accounts')
+    .where({id})
+    .update(accountUpdate)
+    .then(count => {
+        if(count > 0){
+            res.status(200).json({data: count})
+        }else{
+            res.status(404).json({message: "That account does not exist"})
+        }
+    })
+    .catch(error => {
+        res.status(500).json({message: "There was an error updating the record"})
+    })
+}) // Checked!
+
 
 function handleError(){
     console.log("error")
